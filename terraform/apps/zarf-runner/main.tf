cb59776e7c8c6ffab47a2e81c9fc8265f04b0a6d
Will Christensen <will.christensen@defenseunicorns.com>
2023-02-16 15:06:42 -0500

    moving ami stuff to data.tf

--- terraform/apps/zarf-runner/main.tf
+++ terraform/apps/zarf-runner/main.tf
@@ -1,33 +1,3 @@
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
-
-data "aws_ami" "amazon-linux-2" {
-  most_recent = true
-  filter {
-    name   = "owner-alias"
-    values = ["amazon"]
-  }
-
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm*"]
-  }
-}
-
 resource "aws_security_group" "instance_security_group" {
   name        = "${var.shortname}_security_group"
   description = "${var.shortname} security groups"
