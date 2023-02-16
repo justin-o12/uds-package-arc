58373d50a1d8f126b975f3e6d0bf60ba6dd5deb7
Will Christensen <will.christensen@defenseunicorns.com>
2023-02-15 19:27:43 -0500

    Replace raw account number from sts information

--- terraform/apps/tfinit/importer.sh
+++ terraform/apps/tfinit/importer.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ACCOUNT_NUMBER=810783286427
+ACCOUNT_NUMBER=$(aws sts get-caller-identity --query Account --output text)
 
 terraform import aws_s3_bucket.tfbucket tfstate-dd-kb
 terraform import aws_dynamodb_table.action_locktable tfstate-dd-kb-table 
