b608ab4d40eea3c226488f06915a30db4204dd77
Will Christensen <will.christensen@defenseunicorns.com>
2023-02-17 12:13:15 -0500

    fix first line

--- terraform/apps/tfinit/importer.sh
+++ terraform/apps/tfinit/importer.sh
@@ -1,4 +1,4 @@
-:!/bin/bash
+#!/bin/bash
 ACCOUNT_NUMBER=$(aws sts get-caller-identity --query Account --output text)
 
 terraform import aws_s3_bucket.tfbucket                               tfstate-dd-kb
