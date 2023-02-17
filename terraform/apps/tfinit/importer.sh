1f450d43a7a614d1b1b683295a8f1b609275c7e7
Will Christensen <will.christensen@defenseunicorns.com>
2023-02-17 12:11:37 -0500

    tabs to spaces

--- terraform/apps/tfinit/importer.sh
+++ terraform/apps/tfinit/importer.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+:!/bin/bash
 ACCOUNT_NUMBER=$(aws sts get-caller-identity --query Account --output text)
 
-terraform import aws_s3_bucket.tfbucket 							tfstate-dd-kb
-terraform import aws_dynamodb_table.action_locktable 				tfstate-dd-kb-table 
-terraform import aws_iam_instance_profile.edafos_instance_profile 	edafos-Profile
-terraform import aws_iam_role.edafos-instance 						edafos-role
-terraform import aws_iam_policy.s3_bucket 							arn:aws:iam::${ACCOUNT_NUMBER}:policy/edafos-s3-policy 
-terraform import aws_iam_policy.dynamo-rw 							arn:aws:iam::${ACCOUNT_NUMBER}:policy/edafos-dynamo-policy
+terraform import aws_s3_bucket.tfbucket                               tfstate-dd-kb
+terraform import aws_dynamodb_table.action_locktable                  tfstate-dd-kb-table 
+terraform import aws_iam_instance_profile.edafos_instance_profile     edafos-Profile
+terraform import aws_iam_role.edafos-instance                         edafos-role
+terraform import aws_iam_policy.s3_bucket                             arn:aws:iam::${ACCOUNT_NUMBER}:policy/edafos-s3-policy 
+terraform import aws_iam_policy.dynamo-rw                             arn:aws:iam::${ACCOUNT_NUMBER}:policy/edafos-dynamo-policy
 
